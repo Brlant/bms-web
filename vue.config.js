2b696f8413c6db299996de988e73e3a6604300f0
zaoyu63 <zaoyu63@163.com>
2019-09-10 18:54:22 +0800

    [修复]ts-282 项目管理, 编辑导致新增问题

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
     disableHostCheck: true,
     proxy: {
       '/api': {
-        target: 'http://192.168.2.128:8081',
+        target: 'https://bms.cnvss.net/',
         changOrigin: true
       }
     }
