9981f8d7aec519f66c9f0f9f46e6014a700f4f41
zaoyu63 <zaoyu63@163.com>
2019-11-20 16:22:59 +0800

    货值占比计费项不显示问题

--- src/store.js
+++ src/store.js
@@ -49,7 +49,7 @@ const state = {
         {name: '整件入库基本单位数', id: 'A14', unit: '元/基本单位', rule: '每个品种的基本单位总数 X 计费单价', bind: true},
         {name: '散件入库基本单位数', id: 'A5', unit: '元/基本单位', rule: '每个品种的基本单位总数 X 计费单价', bind: true},
         {name: '入库基本单位数', id: 'A18', unit: '元/基本单位', rule: '每个品种的基本单位总数（包含整散） X 计费单价', bind: true},
-        {name: '货品明细货值占比', id: 'A16', unit: '%', rule: '每条订单明细单价 X 货品数量 X 货品明细货值占比', bind: false},
+        {name: '货品明细货值占比', id: 'A16', unit: '%', rule: '每条订单明细单价 X 货品数量 X 货品明细货值占比', bind: true},
       ]
     },
     {
@@ -66,9 +66,9 @@ const state = {
         {name: '整件出库整件数量', id: 'A7', unit: '元/箱', rule: '每个品种的整件总数 X 计费单价', bind: true},
         {name: '整件出库基本单位数', id: 'A15', unit: '元/基本单位', rule: '每个品种的基本单位总数 X 计费单价', bind: true},
         {name: '散件出库基本单位数', id: 'A8', unit: '元/基本单位', rule: '每个品种的基本单位总数 X 计费单价', bind: true},
-        {name: '散件出库周转（包装）箱数', id: 'A9', unit: '元/箱', rule: '每个品种的整件总数 X 计费单价', bind: false},
+        {name: '散件出库周转（包装）箱数', id: 'A9', unit: '元/箱', rule: '每个品种的整件总数 X 计费单价', bind: true},
         {name: '出库基本单位数', id: 'A19', unit: '元/基本单位', rule: '每个品种的基本单位总数（包含整散） X 计费单价', bind: true},
-        {name: '货品明细货值占比', id: 'A17', unit: '%', rule: '每条订单明细单价 X 货品数量 X 货品明细货值占比', bind: false},
+        {name: '货品明细货值占比', id: 'A17', unit: '%', rule: '每条订单明细单价 X 货品数量 X 货品明细货值占比', bind: true},
       ]
     },
     {
