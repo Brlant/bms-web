3347e2b69cfc52d62be8031cc0f460d1cc1be914
zaoyu63 <zaoyu63@163.com>
2019-11-27 13:53:18 +0800

    完善大数据量列表加载

--- src/components/common/large-data-list.vue
+++ src/components/common/large-data-list.vue
@@ -1,6 +1,6 @@
 <template>
   <div :style="`height: ${height}px;overflow-y:auto`" @scroll="scrollEvent">
-    <slot :children="children">sdfsdf</slot>
+    <slot :children="children"></slot>
   </div>
 </template>
 <script>
@@ -13,16 +13,16 @@
       },
       limit: {
         type: Number,
-        default: 50
+        default: 100
       },
       dataList: Array,
       pageSize: {
         type: Number,
-        default: 10
+        default: 50
       },
       lineWidth: {
         type: Number,
-        default: 50
+        default: 55
       }
     },
     data() {
@@ -35,7 +35,7 @@
     watch: {
       dataList(val) {
         if (!val) return;
-        if (val.length < this.limit) {
+        if (val.length <= this.limit) {
           this.children = this.dataList;
         }
         this.children = this.dataList.slice(0, this.limit);
@@ -46,39 +46,54 @@
     },
     methods: {
       scrollEvent(e) {
-        if (this.dataList.length < this.limit) return;
+        if (this.dataList.length <= this.limit) return;
         if (this.loading) return;
         let preScrollTop = this.preScrollTop;
         let curScrollTop = e.target.scrollTop;
         this.preScrollTop = curScrollTop;
         let direction = curScrollTop - preScrollTop;
+        this.loading = true;
         let dif = this.$el.scrollHeight - curScrollTop - this.height;
         let pageSize = this.pageSize;
         if (direction > 0) {
-          if (dif > 100) return;
+          if (dif > 100) {
+            this.loading = false;
+            return;
+          }
           let index = this.dataList.indexOf(this.children.pop());
+          if (index === this.dataList.length - 1) {
+            this.loading = false;
+            return;
+          }
           let list = this.dataList.slice(index, index + pageSize);
-          if (!list.length) return;
-          this.loading = true;
+          if (!list.length) {
+            this.loading = false;
+            return;
+          }
           this.children.push(...list);
           this.children = this.children.slice(list.length - 1);
           setTimeout(() => {
             this.$emit('change');
-            e.target.scrollTop = e.target.scrollTop - pageSize * this.lineWidth;
+            e.target.scrollTop = (this.$el.scrollHeight - this.height) / 2 - this.height;
             this.loading = false;
           }, 100);
         } else {
-          if (curScrollTop > 100) return;
+          if (curScrollTop > 100) {
+            this.loading = false;
+            return;
+          }
           let index = this.dataList.indexOf(this.children[0]);
           let start = index - pageSize > 0 ? index - pageSize : 0;
           let list = this.dataList.slice(start, index);
-          if (!list.length) return;
-          this.loading = true;
+          if (!list.length) {
+            this.loading = false;
+            return;
+          }
           this.children = list.concat(this.children);
           this.children = this.children.slice(0, this.children.length - pageSize);
           setTimeout(() => {
             this.$emit('change');
-            e.target.scrollTop = e.target.scrollTop + pageSize * this.lineWidth;
+            e.target.scrollTop = (this.$el.scrollHeight - this.height) / 2 + this.height;
             this.loading = false;
           }, 100);
         }
