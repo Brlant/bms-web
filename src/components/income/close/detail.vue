1d7498b0eb0f5308d00726118a9582fced5ff6bb
zaoyu63 <zaoyu63@163.com>
2020-02-21 17:05:40 +0800

    回款日期显示时分秒

--- src/components/income/close/detail.vue
+++ src/components/income/close/detail.vue
@@ -48,7 +48,7 @@
           <oms-row label="结算单金额" :span="8">{{ formItem.statementAmount  | formatMoney}}</oms-row>
           <oms-row label="结算日期" :span="8">{{ formItem.statementTime | time }}</oms-row>
           <oms-row label="待回款金额" :span="8">{{ formItem.unreturnedAmount  | formatMoney}}</oms-row>
-          <oms-row label="回款日期" :span="8" v-show="formItem.backAmountTime">{{ formItem.backAmountTime | time }}</oms-row>
+          <oms-row label="回款日期" :span="8" v-show="formItem.backAmountTime">{{ formItem.backAmountTime | date }}</oms-row>
           <oms-row label="优惠金额" :span="8" v-show="formItem.preferentialAmount">{{ formItem.preferentialAmount  | formatMoney}}
           </oms-row>
           <oms-row label="折扣" :span="8" v-show="formItem.discountAmount">{{ formItem.discountAmount }}%</oms-row>
